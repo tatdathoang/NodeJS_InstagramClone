19c62b25cc8f05250ddb24aacc7c0274252cf78e
Dat Hoang <tatdathoang@gmail.com>
2017-07-12 17:05:49 +0000

    Create the hash file and the sendmail function

--- utils/hash.js
+++ utils/hash.js
@@ -1,9 +1,11 @@
 const bCrypt = require('bcrypt-nodejs');
 
-module.exports.createHash = function(password){
+module.exports.createHash = function(password)
+{
         return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
-    }
+}
     
-module.exports.isValid = function(user, password){
+module.exports.isValid = function(user, password)
+{
         return bCrypt.compareSync(password, user.password);
-    }
+}
--- utils/sendmail.js
+++ utils/sendmail.js
@@ -1,20 +1,23 @@
 const nodemailer = require('nodemailer');
 
-module.exports.send = function(to, subject, text){
-    
+module.exports.send = function(to, subject, text)
+{
     // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
+    let transporter = nodemailer.createTransport(
+        {
         host: 'smtp.gmail.com',
         port: 465,
         secure: true, // secure:true for port 465, secure:false for port 587
-        auth: {
+        auth: 
+        {
             user: 'prog8165@gmail.com',
             pass: 'Prog8165!'
         }
     });
     
     // setup email data with unicode symbols
-    let mailOptions = {
+    let mailOptions = 
+    {
         from: '"Instagram Clone" <prog8165@gmail.com>', // sender address
         to: to, // list of receivers
         subject: subject, // Subject line
@@ -22,8 +25,10 @@ module.exports.send = function(to, subject, text){
     };
     
     // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
+    transporter.sendMail(mailOptions, (error, info) => 
+    {
+        if (error) 
+        {
             return console.log(error);
         }
         console.log('Message %s sent: %s', info.messageId, info.response);
